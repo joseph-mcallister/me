fb78e5835c52201fe293e2b62eed65494e734a94
Joseph McAllister <joseph.l.mcallister@gmail.com>
2020-10-13 11:43:56 -0500

    last tweaks

--- portfolio/src/mock/data.js
+++ portfolio/src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Hi, my name is Joe McAllister.', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Entrepenuer & Engineer', // e.g: Welcome to my website
 };
 
 // HERO DATA
